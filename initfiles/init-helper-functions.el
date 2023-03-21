7d87bacd7adcbeeb4fb88cabff69d951136e3736
Alan <1418294+alangalvino@users.noreply.github.com>
2023-03-21 14:29:05 -0300

    Add kill-other-buffers and find-file helpers

--- initfiles/init-helper-functions.el
+++ initfiles/init-helper-functions.el
@@ -1,3 +1,15 @@
+(defun find-file-or-projectile ()
+  "If on projectile tries to find file with projectile if not with helm"
+  (interactive)
+  (if (projectile-project-p)
+      (call-interactively 'helm-projectile-find-file)
+    (call-interactively 'helm-find-files)))
+
+(defun kill-other-buffers ()
+  "Kill all other buffers."
+  (interactive)
+  (mapc 'kill-buffer (delq (current-buffer) (buffer-list))))
+
 (defun server-shutdown ()
   "Save buffers, quit, and shutdown (kill) server"
   (interactive)
